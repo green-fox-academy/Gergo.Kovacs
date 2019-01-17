145a5ea61e682d95856df61d643fa67523d1ec15
gergo.kovacs <koge726@yahoo.com>
2019-01-17 18:09:20 +0100

    week 1 day 4 feladat 2 refactor

--- week-01/day-4/feladat2/main.cpp
+++ week-01/day-4/feladat2/main.cpp
@@ -6,13 +6,12 @@ void greet(std::string);
 
 int main(int argc, char* args[]) {
 
+    std::string a5= "Greenfox";
+    greet(a5);
+    
     // - Create a string variable named `al` and assign the value `Greenfox` to it
-    std::string a1= "Greenfox";
-    greet(a1);
     // - Create a function called `greet` that greets it's input parameter
-
     // - Greeting is printing e.g. `Greetings dear, Greenfox`
-
     // - Greet `al`
 
     return 0;
