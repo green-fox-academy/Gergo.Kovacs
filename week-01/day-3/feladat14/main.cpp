253ac91dff7a5ea80792ce09cf490123aa44f8dd
gergo.kovacs <koge726@yahoo.com>
2019-01-18 15:07:57 +0100

    C++ week-01 day3 feladat14

---
+++ week-01/day-3/feladat14/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main(int argc, char* args[]) {
+    std::string nev;
+    // Modify this program to greet user instead of the World!
+    // The program should ask for the name of the user
+    std::cout << "Hello hogy hívnak?" << std::endl;
+    std::cin>>nev;
+
+    std::cout<<"hello "<< nev<<"!"<<std::endl;
+
+
+    return 0;
+}
