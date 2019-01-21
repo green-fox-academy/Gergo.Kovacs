136c645953b7d860116aae44fc9acd5cbbb28b94
gergo.kovacs <koge726@yahoo.com>
2019-01-21 10:09:44 +0100

    C++ week-01 day3 feladat24

---
+++ week-01/day-3/feladat24/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int a =0;
+
+    while (a <=500){
+        if (a %2 ==0)
+            std::cout<<a<<std::endl;
+        a++;
+    }
+
+    // Create a program that prints all the even numbers between 0 and 500
+
+    return 0;
+}
