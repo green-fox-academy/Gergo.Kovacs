257391c097fcf533a03c764188cd4be8e1a43ea1
gergo.kovacs <koge726@yahoo.com>
2019-01-21 10:01:51 +0100

    C++ week-01 day3 feladat22

--- week-01/day-3/feladat22/main.cpp
+++ week-01/day-3/feladat22/main.cpp
@@ -3,48 +3,77 @@
 
 int main(int argc, char* args[]) {
 
-    double a = 24;
+    int a=24;
     int out = 0;
     // if a is even increment out by one
 
-    if(a%2==0){
-        out++;
+
+    if (a%2==0){
+        std::cout << out++ << std::endl;
+    } else {
+        std::cout << out << std::endl;
     }
-    std::cout << out << std::endl;
+
+    int b = 9;
+    std::string out2 ="sweet";
+    std::string out3 ="less";
+    std::string out4 ="more";
+
+    if ((10 <= b) && (a<=20)){
+        std::cout<< out2 <<std::endl;
+    }
+    else if (b<10){
+        std::cout<< out3 <<std::endl;
+    }
+    else if (b>20){
+        std::cout<< out4 <<std::endl;
+    }
 
 
-    int b = 13;
-    std::string out2 = "";
     // if b is between 10 and 20 set out2 to "Sweet!"
     // if less than 10 set out2 to "Less!",
     // if more than 20 set out2 to "More!"
 
-    std::cout << out2 << std::endl;
+
 
 
     int c = 123;
-    int credits = 100;
+    int credits = 20;
     bool isBonus = false;
+    int c1;
+    int c2;
+    c1=c-2;
+    c2=c-1;
+
+    if ((credits >= 50) && (isBonus=false)){
+
+        std::cout<<c1<<std::endl;
+
+    } else if ((credits <50) && (isBonus=false)){
+        std::cout<<c2<<std::endl;
+    } else if (isBonus=true){
+        std::cout<<c<<std::endl;
+    }
     // if credits are at least 50,
     // and isBonus is false decrement c by 2
     // if credits are smaller than 50,
     // and isBonus is false decrement c by 1
     // if isBonus is true c should remain the same
 
-    std::cout << c << std::endl;
-
-
-    int d = 8;
-    int time = 120;
-    std::string out3 = "";
-    // if d is dividable by 4
-    // and time is not more than 200
-    // set out3 to "check"
-    // if time is more than 200
-    // set out3 to "Time out"
-    // otherwise set out3 to "Run Forest Run!"
-
-    std::cout << out3 << std::endl;
-
     return 0;
+}
+
+int d = 8;
+int time = 120;
+std::string out3 = "";
+// if d is dividable by 4
+// and time is not more than 200
+// set out3 to "check"
+// if time is more than 200
+// set out3 to "Time out"
+// otherwise set out3 to "Run Forest Run!"
+
+std::cout << out3 << std::endl;
+
+return 0;
 }
