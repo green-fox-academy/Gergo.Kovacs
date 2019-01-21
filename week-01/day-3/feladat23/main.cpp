03e77c76ecbd0157622c479d75aeca785a63a0a0
gergo.kovacs <koge726@yahoo.com>
2019-01-21 10:08:48 +0100

    C++ week-01 day3 feladat23

---
+++ week-01/day-3/feladat23/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+
+    for(int i = 0; i<10;i++){
+        std::cout << "I won't cheat on the exam!" << std::endl;
+    }
+
+    return 0;
+}
